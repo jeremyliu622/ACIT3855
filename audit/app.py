eff6429484814500a90b59e2b0d22c07950afb21
Jeremy <boliu622@hotmail.com>
2021-11-23 10:07:43 -0800

    'lab10'

--- audit/app.py
+++ audit/app.py
@@ -88,9 +88,13 @@ def get_purchase_history(index):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-CORS(app.app)
-app.app.config['CORS_HEADERS']='Content-Type'
+
+if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
+    CORS(app.app)
+    app.app.config['CORS_HEADERS']='Content-Type'
+
 app.add_api('openapi.yaml',
+            base_path="/audit_log",
             strict_validation=True,
             validate_responses=True)
 
--- receiver/app.py
+++ receiver/app.py
@@ -95,6 +95,7 @@ def purchase_book(body):
 
 app = connexion.FlaskApp(__name__, specification_dir='')
 app.add_api('openapi.yaml',
+            base_path="/receiver",
             strict_validation=True,
             validate_responses=True)
 
